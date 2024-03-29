8c390a47bff3546be6c63bdaee1caa8fbccc479e
johnson williams <williams.gb55@gmail.com>
2024-03-29 13:07:01 +0900

    newUpdate

--- src/App.js
+++ src/App.js
@@ -1,64 +1,74 @@
 import "./App.css";
 import React from "react";
 import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faEnvelope,
+  faFaceSmile,
+  faHeart as solidHeart,
+} from "@fortawesome/free-solid-svg-icons";
+import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
 
-function getRandomColor() {
-  // Generate random values for red, green, and blue components
-  var r = Math.floor(Math.random() * 256); // Random value between 0 and 255
-  var g = Math.floor(Math.random() * 256); // Random value between 0 and 255
-  var b = Math.floor(Math.random() * 256); // Random value between 0 and 255
-
-  // Construct the RGB color string
-  var color = "rgb(" + r + "," + g + "," + b + ")";
-  return color;
-}
-
-const buttonsInfo = [
-  { id: crypto.randomUUID(), text: "Click me", color: "#541388" },
-  { id: crypto.randomUUID(), text: "Click him", color: "#d90368" },
-  { id: crypto.randomUUID(), text: "Click her", color: "#2e294e" },
-];
+library.add(faFaceSmile);
 
 function App() {
-  const [bgColor, setBgColor] = useState("#dae7f5");
+  const [likes, setLikes] = useState(false);
+  const [count, setCount] = useState(0);
 
-  const handleElements = (id) => {
-    buttonsInfo.map((a) => {
-      return a.id === id
-        ? ((a.color = getRandomColor()), setBgColor(getRandomColor()))
-        : null;
-    });
+  const handleLikes = () => {
+    setLikes((a) => !a);
+    setCount((b) => (likes ? (b = 0) : (b = +1)));
   };
-
   return (
-    <div style={{ backgroundColor: bgColor }}>
-      <Title />
-      {buttonsInfo.map(({ id, text, color }) => {
-        return (
-          <Button
-            id={id}
-            text={text}
-            handleElements={handleElements}
-            buttonColor={color}
-          />
-        );
-      })}
+    <div className="container">
+      <Header count={count} />
+      <BodyCard likes={likes} handleLikes={handleLikes} />
     </div>
   );
 }
 
-function Title() {
-  return <h1>Randomizer</h1>;
+function Header({ count }) {
+  return (
+    <div className="text-center">
+      <h1 className="display-3">Double Click To Like</h1>
+      <h1 className="display-6">Like {count}</h1>
+    </div>
+  );
 }
 
-function Button({ id, handleElements, buttonColor, text }) {
+function BodyCard({ likes, handleLikes }) {
   return (
-    <button
-      style={{ backgroundColor: buttonColor }}
-      onClick={() => handleElements(id)}
-    >
-      {text}
-    </button>
+    <div className="card mx-auto" style={{ width: "18rem", height: "65vh" }}>
+      <div class="row p-2">
+        <div className="col-sm-auto">
+          <FontAwesomeIcon icon={faFaceSmile} size="2x" />
+        </div>
+        <div className="col">
+          <h5 className="card-title">Lost Lucky</h5>
+        </div>
+      </div>
+
+      <img
+        src="https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J"
+        alt="dog"
+        style={{ height: "100%", objectFit: "cover" }}
+        onDoubleClick={() => handleLikes()}
+      />
+      <div class="row p-2">
+        <div className="col-sm-auto">
+          <FontAwesomeIcon icon={faEnvelope} size="2x" />
+        </div>
+        <div className="col text-end">
+          <FontAwesomeIcon
+            onClick={() => handleLikes()}
+            icon={likes ? solidHeart : regularHeart}
+            size="2x"
+          />
+        </div>
+      </div>
+    </div>
   );
 }
 
